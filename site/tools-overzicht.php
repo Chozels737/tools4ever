d4db01b42e1e9beb66c7a4e21d23e4d68a96062e
Yigit Uzel <pcdt911@gmail.com>
2023-04-12 11:41:09 +0200

    save

--- site/tools-overzicht.php
+++ site/tools-overzicht.php
@@ -25,10 +25,48 @@ $all_tools = mysqli_fetch_all($result, MYSQLI_ASSOC);
  * maar je kunt ook andere HTML-**tags** gebruiken
  */
 ?>
-<?php foreach($all_tools as $tool): ?>
-  <p><?php echo $tool["tool_id"] ?></p>
-  <p><?php echo $tool["tool_name"] ?></p>
-  <p><?php echo $tool["tool_category"] ?></p>
-  <p><?php echo $tool["tool_price"] ?></p>
-  <p><?php echo $tool["tool_brand"] ?></p>
-<?php endforeach; ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div class="title">
+        <h1>overzicht</h1>
+    </div>
+
+    <div class="tabl">
+
+    <table>
+    <tr>
+        <th>product nummer</th>
+        <th>naam</th>
+        <th>categorie</th>
+        <th>prijs</th>
+        <th>merk</th>
+    </tr>
+
+    <?php foreach($all_tools as $tool): ?>
+        <tr>
+            <td><?php echo $tool["tool_id"] ?></td>
+            <td><?php echo $tool["tool_name"] ?></td>
+            <td><?php echo $tool["tool_category"] ?></td>
+            <td><?php echo $tool["tool_price"] ?></td>
+            <td><?php echo $tool["tool_brand"] ?></td>
+        </tr>
+        <?php endforeach; ?>
+
+    </table>    
+    </div>
+
+
+
+</body>
+</html>
